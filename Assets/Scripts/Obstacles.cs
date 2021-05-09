add89e29f4aa837e91f9244ee7aa581bb1ecb26f
James O'Callaghan <psybergamer@gmail.com>
2021-05-09 12:08:42 +0100

    Obstacle Class Added and Safe Class Obstacle Added.
    Obstacle class is super class for all obstacle in games.
    Safe Obstacle class for handle power-ups and bonuses etc.

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public virtual void Interaction()
+    {
+        //this is the function to overide for obstacle functionality.
+    }
+}
---
+++ Assets/Scripts/Safe.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Safe : Obstacles
+{
+    private enum ObstacleType { SHOOTINGSTAR, SHIMMERINGSTAR, HALODSTAR, VALUABLEMETORITE }
+    [SerializeField]
+    private ObstacleType _type;
+    public override void Interaction()
+    {
+        //what type object are we interacting with
+        switch (_type)
+        {
+            case ObstacleType.SHOOTINGSTAR:
+                //speed boost for a duration of 3 seconds
+                //
+                //call function in player called speed boost
+                break;
+            case ObstacleType.SHIMMERINGSTAR:
+                //invicibilty for a duration of 3 seconds
+                //
+                //call function in player called invicibilty
+                break;
+            case ObstacleType.HALODSTAR:
+                //double points 1 minute duration
+                //call function in player called double points
+                break;
+            case ObstacleType.VALUABLEMETORITE:
+                //points boost extra points based on multiplier.
+                //
+                //add points in highscore.
+                break;
+            default:
+                break;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //call interaction
+        Interaction();
+    }
+}
